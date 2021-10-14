ec40bc066a37963d7a008ae33690c00b248e42e9
Don <appleonpoint@gmail.com>
2021-10-14 12:03:47 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+function addCrack() {
+  console.log('you added crack to your sandwich')
+  let listElem = document.getElementById('toppings-list')
+  listElem.innerHTML += '<li> Crack</li>'
+}
+
+//SECTION Functional
+
+function addPb() {
+  console.log('you added peanut butter to your sandwich')
+  let listElem = document.getElementById('toppings-list')
+  listElem.innerHTML += '<li> Peanut Butter</li>'
+}
+
+function addRit() {
+  console.log('you added Ritalin to your sandwich')
+  let listElem = document.getElementById('toppings-list')
+  listElem.innerHTML += '<li> Ritalin</li>'
+}
+
+function addAddy() {
+  console.log('you added Adderall to your sandwich')
+  let listElem = document.getElementById('toppings-list')
+  listElem.innerHTML += '<li> Adderall</li>'
+}
